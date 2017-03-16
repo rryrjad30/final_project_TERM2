c9fcdd85bd9caa890aa30bcbf7b6926417e82364
rryrrjad30 <pierry27@gmail.com>
2017-03-17 01:16:22 +0700

    dialog login username dan password

--- src/FrmMain.java
+++ src/FrmMain.java
@@ -182,7 +182,7 @@ public class FrmMain extends javax.swing.JFrame {
     }
     private void executeLogout() {
         this.setVisible(false);
-        FrmDialogLogin.instance.setVisible(true);
+        JDialogLogin.instance.setVisible(true);
     }
     private void ExecuteAbout(){
          Sutil.msg(this, "Book Forest Library"
@@ -191,33 +191,6 @@ public class FrmMain extends javax.swing.JFrame {
                 + "\n               Pierry Rajadi"
                 + "\n               Valerie Leonie"
                 + "\n               William");
-    }
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-           String LAF_QUAQUA_OSX = "ch.randelshofer.quaqua.QuaquaLookAndFeel";
-            javax.swing.UIManager.setLookAndFeel(LAF_QUAQUA_OSX);
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FrmMain().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
